4728d62290c0b82f0b751fb1ff6e96fe701a6f82
lijiawei <jawoo@qq.com>
2023-06-09 22:33:28 +0800

    feat: egg

---
+++ app/schedule/hzjyksy.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+const ChatBot = require('dingtalk-robot-sender');
+
+module.exports = {
+  schedule: {
+    immediate: true,
+    interval: '60m',
+    type: 'worker',
+    disable: process.env.NODE_ENV !== 'production',
+  },
+  async task(ctx) {
+    const url = 'http://www.hzjyksy.cn/';
+    const name = 'hzjyksy';
+    const cache = ctx.app.cache[name] ?? [];
+    const browser = await puppeteer.launch({ timeout: 60000 });
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector('.article_tab');
+    const texts = await page.$$eval('.article_tab > div.bd > div > div.con.cms_show > ul li', elements => elements.map(element => element.children[1].textContent.replace('\n', '(' + element.children[2].textContent + ')')));
+    ctx.app.cache[name] = texts;
+    const news = texts.filter(text => cache.indexOf(text) === -1);
+    if (news.length > 0) {
+      const robot = new ChatBot({
+        baseUrl: 'https://oapi.dingtalk.com/robot/send',
+        accessToken: '51a16886f61de68e727485c5d678db2315ddc49ac89e6879bde6fb639b596a23',
+        secret: 'SEC43537ccc51eb5989e1a1b47c8bae9d5d2c74d4d6ae8429ea6da1f4740bc8b8dd',
+      });
+      const content = news.map(text => `${text}\n`) + '\n\n' + url;
+      robot.markdown(name, content);
+    }
+  },
+};
---
+++ config/config.default.js
@@ -0,0 +1,18 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {
+    keys: appInfo.name + '_1686315269579_1405'
+  };
+
+  return config;
+};
