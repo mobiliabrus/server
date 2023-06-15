0912aa0b42ac3b42ca9e48f4e2b97b808b28bb08
mobiliabrus <97830321+mobiliabrus@users.noreply.github.com>
2023-06-15 09:26:38 +0800

    Update hzjyksy.js

--- app/schedule/hzjyksy.js
+++ app/schedule/hzjyksy.js
@@ -15,6 +15,7 @@ module.exports = {
     await page.goto(url);
     await page.waitForSelector('.article_tab');
     const texts = await page.$$eval('.article_tab > div.bd > div > div.con.cms_show > ul li', elements => elements.map(element => element.children[1].textContent.replace('\n', '(' + element.children[2].textContent + ')')));
+    await browser.close();
     ctx.app.cache[name] = texts;
     const news = texts.filter(text => cache.indexOf(text) === -1);
     if (news.length > 0) {
